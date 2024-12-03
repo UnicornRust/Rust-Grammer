d5580d462f1f9baa1150334581879e7aee2119b2
ancion <ancion@github.com>
2024-12-03 23:13:44 +0800

    trait object

--- src/traits/embed.rs
+++ src/traits/embed.rs
@@ -1,5 +1,118 @@
+//
+// Copy and Clone trait
+//
+// 大部分的固定大小类型都实现了 Copy trait, 赋值行为发生时进行了值的复制
+
+fn copy_clone() {
+    // 
+    let owner_one = 32;
+    let owner_two = owner_one;
+
+    println!("one: {}, two: {}", owner_one, owner_two);
+
+    // 对于一些动态大小的类型，比如 str, [T], 在使用他们的指针 String, Vec<T> 时
+    // 不会发生只的复制，而是发生所有权的转移
+
+    let owner_one = String::from("hello");
+    let owner_two = owner_one;
+
+    // 所有权发生转移后 owner_one 已经被标记为无效了      
+    // println!("one: {}, two: {}", owner_one, owner_two);
+
+
+    // 从 trait 角度来讲，就是固定大小类型实现了 Copy trait 和 Clone trait, 
+    // 动态大小类型没有实现 Copy trait, 大多数实现了 Clone trait
+    // 经常在一些错误使用的时候，编译器会告诉你，这些类型没有实现 Copy trait
+    
+    // 如果你想要在堆上赋值, 想像固定大小类型一样复制一份数据，
+    // 可以显示调用 Clone trait de  clone() 方法
+
+
+    // clone 出的数据都有自己的所有权，都是独立有效的, 不会发生所有权转移 
+    let v = vec![1, 2, 3, 4, 5];
+    let v1 = v.clone();
+    let v2 = v1.clone();
+    let v3 = v.clone();
+
+}
+
+
+// 类型转换实例 From 和 Into Trait
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number  {
+    fn from (item: i32) -> Self {
+        Number { value: item}
+    }
+}
 
 pub fn from() {
+
+    // 使用 From trait 中的 from 方法实现 i32 类型向 Number 类型的转换
+    let num = Number::from(10);
+    println!("Number is {:?}", num);
+
+    let number = Number { value : 10};
+    let i = i32::from(number);
+    println!("i is : {}", i);
+    
+}
+
+
+impl From<Number> for i32 {
+    fn from(value: Number) -> Self {
+        value.value
+    }
+}
+
+pub fn into() {
+
+    // 实现了 From trait 的类型就自动实现了 Into trait, 也可以使用 into() 方法
+    // 注意使用 into() 的时候需要明确的指定返回的类型
+    let n: Number = 23.into();
+    println!("Number is {:?}", n);
+
+    let number = Number { value : 10};
+    let i: i32 = number.into();
+    println!("i is : {}", i);
+
+}
+
+// 与 From 和 Into trait 类型的 还有 TryFrom 和 TryInto
+// 在这个 trait 的实现中，增加了对错误的处理
+// 业务场景中使用的更多
+
+
+// AsRef 和 AsMut Trait
+//
+// 通过 AsMut 可以获取结果提成员的可变引用
+impl AsMut<i32> for Number {
+    fn as_mut(&mut self) -> &mut i32 {
+        &mut self.value
+    }
+}
+
+fn as_ref_mut() {
+    let mut number = Number{ value: 20};
+    let ref_num = number.as_mut();
+    *ref_num += 10;
+    println!("number is : {}", number.value);
+
+    // 不可变引用
+    let num = Number { value : 40};
+    let ref_num = num.as_ref();
+    println!("ref_num is : {}", *ref_num);
+}
+
+// 可以获取成员的不可变应用
+impl AsRef<i32> for Number {
+    fn as_ref(&self) -> &i32 {
+        &self.value
+    }
 
 }
 
--- src/traits/objects.rs
+++ src/traits/objects.rs
@@ -1,3 +1,108 @@
+// trait 对象
+// ------------------------------------------------------------------------
+// trait 对象的使用
+//
+// trait 对象通过指针来创建，如 & 或者 Box<T>(一种智能指针, 可以把数据存储在堆上)
+// &dyn Trait or Box<dyn Trait>
+// Box 是 Rust 中唯一一个可以直接把数据分配到堆上的类型
+
+trait Animal {
+    fn make_sound(&self) -> &str;
+}
+
+struct Dog;
+
+impl Animal for Dog {
+    fn make_sound(&self) -> &str{
+        "woof"
+    }
+}
+
+
+struct Cat;
+
+impl Animal for Cat {
+    fn make_sound(&self) -> &str{
+        "meow"
+    }
+}
 
 pub fn union()  {
+
+    let d = Dog;
+    let c = Cat;
+
+    let animals: Vec<&dyn Animal> = vec![&d, &c];
+    animal_make_sound(animals);
+
 }
+
+
+// 通过引用和 trait 的方式来实现运行时的动态分发
+// 因为 trait 对象的限定在编译期不能判断类型
+// 因此在运行时需要先判定类型，然后根据类型来查找对应的方法，
+// 这样会带来一定的运行时性能开销，但是灵活性更好
+fn animal_make_sound(animals:  Vec<&dyn Animal>) {
+    for ele in animals {
+        ele.make_sound();
+    }
+}
+
+// 3. trait object 安全 
+// --------------------------------------------------------------
+// (不是所有的trait都可以创建 trait object) 以下两种情况之外的类型都不能创建 trait 对象
+// trait 中方法返回类型不为 Self
+// trait 中方法不存在泛型类型参数
+//
+
+pub trait Yris {
+    fn method(&self) -> Self;
+}
+
+// 泛型参数的trait
+pub trait Xris {
+    fn print<T: std::fmt::Display>(&self, t:T);
+}
+
+// 无法使用 trait object
+// fn use_trait_object(t: &dyn Xris) {}
+//  fn use_trait_object(t: &dyn Yris) {}
+
+
+
+
+// 4. trait 实现与所有权
+//
+// 在自定义的 trait 方法中, 可以根据需要获取类型的不可变引用，可变引用或者所有权
+
+// 所有权机制和 trait 本质上是两个独立的概念，即使没有 trait, 所有权机制也是成立的
+// 这也就是在前面讨论所有权机制的时候并没有提及 trait,
+// 但是 trait 让所有权机制更加显示化，更好理解，更好使用
+
+trait A {
+    // 需要手动实现，获取所有权
+    // 对象执行此方法之后，将移交所有权，则对象不能再被使用
+    fn take_ownership(self);
+
+    // 默认实现，获取不可变引用
+    fn take_ref(&self) {
+        println!("此方法获取了不可变引用");
+    }
+
+    // 默认实现，获取可变引用
+    fn take_mut_ref(&mut self) {
+        println!("此方法获取了可变引用");
+    }
+}
+
+struct X;
+
+impl A for X {
+
+    fn take_ownership(self) {
+        println!("方法获取到类型的所有权");
+    }
+    // 默认实现不用手动实现
+}
+
+
